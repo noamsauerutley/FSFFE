d9f6776281a5c241be433b3803b4b8d8329e95a6
noamsauerutley <noamsauerutley@gmail.com>
2020-08-29 02:59:42 +0000

    Add route

--- app.js
+++ app.js
@@ -5,4 +5,10 @@ app.get('/', (req, res) => {
 	res.send('up & running :)');
 });
 
+app.get('/teapot', (req, res) => {
+	res.set('X-pot-type', 'coffee');
+	res.status(418);
+	res.send('jk actually coffee');
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
